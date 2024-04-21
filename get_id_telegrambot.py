5ae591fed047c9644b65ea3bdd65dac0a002257a
Luis Enrique <47234519+enriluis@users.noreply.github.com>
2024-04-21 13:31:48 -0400

    Update get_id_telegrambot.py
    hide  telegram bot id

--- get_id_telegrambot.py
+++ get_id_telegrambot.py
@@ -7,7 +7,7 @@ def obtener_id_de_chat(update, context):
     context.bot.send_message(chat_id=chat_id, text=f"El ID de chat es: {chat_id}")
 
 # Token de tu bot
-TOKEN = "6563344306:AAEXTdxkC1btSc-C2nV3cfts6r5ZSy_ABGw"
+TOKEN = "***********************"
 
 # Crear el objeto updater y dispatcher
 updater = Updater(TOKEN, use_context=True)
